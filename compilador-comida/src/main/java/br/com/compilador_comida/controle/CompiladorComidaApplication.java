3ffa3c2972f4d5b84fd0821cec6672bb5ded7ae3
Jian Licio de Oliveira <jian_oliveira12@hotmail.com>
2025-01-24 18:57:40 -0300

    Mudanças drasticas
    Está sendo utilizado:
    VS Code
    Spring Boot
    Vue

---
+++ compilador-comida/src/main/java/br/com/compilador_comida/controle/CompiladorComidaApplication.java
@@ -0,0 +1,11 @@
+package br.com.compilador_comida.controle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompiladorComidaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CompiladorComidaApplication.class, args);
+    }
+}
---
+++ compilador-comida/src/test/java/br/com/compilador_comida/CompiladorComidaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compilador_comida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompiladorComidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
